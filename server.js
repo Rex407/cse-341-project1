17217da667fa39c0686ccafa3dedc0f54bceaa82
Rex407 <rexkapolom@outlook.com>
2026-03-13 13:22:29 +0200

    Fix Swagger server URL

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
-app.use(cors())
 
 const contactsRoutes = require("./routes/contactsRoutes")
 
+app.use(cors())
 app.use(express.json())
 app.use("/contacts", contactsRoutes)
 
