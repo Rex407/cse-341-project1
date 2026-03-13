2062aba4e25514a98d0ae16f9abc9e5af33c8456
Rex407 <rexkapolom@outlook.com>
2026-03-13 13:11:27 +0200

    Enabled CORS for Swagger

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const { MongoClient } = require("mongodb")
+const cors = require("cors") 
 require("dotenv").config()
 
 const app = express()
+app.use(cors())
 
 const contactsRoutes = require("./routes/contactsRoutes")
 
